e3f2f0eea8a5d7a4c962776c58b954b839b722bd
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-04-28 13:45:30 +0100

    Trouble shouting the video stream

--- Programs/Main/Main.ino
+++ Programs/Main/Main.ino
@@ -17,30 +17,43 @@ int ledIntensity = 125;
 char generatedText[30] = "empty";
 uint32_t lastDataMillis = 0;
 
-WebServer server(80);
+// WebServer server(80);
+
+void setupCamera();
+camera_config_t setupConfiguration();
+void connectToWiFi();
 void startCameraServer();
 
 void setup() {
     Serial.begin(115200);
-    Serial.setDebugOutput(true);
-    Serial.println("");
 
-    // disableCore0WDT();
-    // disableCore1WDT();
+    setupCamera();
+    Serial.println("Done setup camera.");
 
-    camera_config_t config = setUpEspPins();
+    connectToWiFi();
+    Serial.println("Done connect WiFi.");
 
-    if (config.pixel_format == PIXFORMAT_JPEG) {
-        if (psramFound()) {
-            config.jpeg_quality = 10;
-            config.fb_count = 2;
-            config.grab_mode = CAMERA_GRAB_LATEST;
-        } else {
-            // Limit the frame size when PSRAM is not available
-            config.frame_size = FRAMESIZE_VGA;
-            config.fb_location = CAMERA_FB_IN_DRAM;
-        }
-    }
+    // server.on("/", sendMainPage);
+    // server.on("/xml", sendXmlData);
+    // server.on("/UPDATE_INTENSITY", sliderIntensityUpdate);
+    // server.on("/TOGGLE_VIEW_STREAM", toggleViewStream);
+    // server.on("/TOGGLE_LISTENING", toggleListening);
+    // server.on("/TOGGLE_TALKING", toggleTalking);
+
+    // server.begin();
+}
+
+void loop() {
+    digitalWrite(33, HIGH);
+    delay(250);
+    digitalWrite(33, LOW);
+    delay(250);
+
+    // server.handleClient();
+}
+
+void setupCamera() {
+    camera_config_t config = setupConfiguration();
 
     esp_err_t err = esp_camera_init(&config);
     if (err != ESP_OK) {
@@ -49,34 +62,11 @@ void setup() {
     }
 
     sensor_t *s = esp_camera_sensor_get();
-    if (config.pixel_format == PIXFORMAT_JPEG) {
-        s->set_framesize(s, FRAMESIZE_QVGA);
-    }
-
-    connectToWiFiSTA();
-
-    server.on("/", sendMainPage);
-    server.on("/xml", sendXmlData);
-    server.on("/UPDATE_INTENSITY", sliderIntensityUpdate);
-    server.on("/TOGGLE_VIEW_STREAM", toggleViewStream);
-    server.on("/TOGGLE_LISTENING", toggleListening);
-    server.on("/TOGGLE_TALKING", toggleTalking);
-
-    server.begin();
+    s->set_framesize(s, FRAMESIZE_VGA);
 }
-
-void loop() {
-    if ((millis() - lastDataMillis) >= 40) {
-        lastDataMillis = millis();
-
-        printData();
-    }
-
-    server.handleClient();
-}
-
-camera_config_t setUpEspPins() {
+camera_config_t setupConfiguration() {
     camera_config_t config;
+
     config.ledc_channel = LEDC_CHANNEL_0;
     config.ledc_timer = LEDC_TIMER_0;
     config.pin_d0 = Y2_GPIO_NUM;
@@ -95,43 +85,26 @@ camera_config_t setUpEspPins() {
     config.pin_sccb_scl = SIOC_GPIO_NUM;
     config.pin_pwdn = PWDN_GPIO_NUM;
     config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.frame_size = FRAMESIZE_UXGA;
-    // QVGA    | CIF     | VGA     | SVGA    | XGA      | SXGA      | UXGA
-    // 320x240 | 352x288 | 640x480 | 800x600 | 1024x768 | 1280x1024 | 1600x1200
+    config.xclk_freq_hz = 10000000;
+    config.frame_size = FRAMESIZE_VGA;
     config.pixel_format = PIXFORMAT_JPEG;
     config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
     config.fb_location = CAMERA_FB_IN_PSRAM;
-    config.jpeg_quality = 12;
+    config.jpeg_quality = 16;
     config.fb_count = 1;
+
     return config;
 }
-
-// void connectToWiFiSAP() {
-//     Serial.println("Starting server...");
-//     WiFi.softAP(ssid, password);
-//     delay(100);
-
-//     WiFi.softAPConfig(PageIP, gateway, subnet);
-//     delay(100);
-
-//     actualIP = WiFi.softAPIP();
-//     Serial.print("IP address: ");
-//     Serial.println(actualIP);
-
-//     startCameraServer();
-// }
-void connectToWiFiSTA() {
+void connectToWiFi() {
     WiFi.begin(ssid, password);
     WiFi.setSleep(false);
 
-    Serial.print("Connecting");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
     Serial.println("");
-    Serial.println("WiFi connected.");
+    Serial.println("WiFi connected");
 
     startCameraServer();
 
@@ -157,61 +130,61 @@ void printData() {
     Serial.println(generatedText);
 }
 
-void sendMainPage() { server.send(200, "text/html", htmlWebPage); }
-void sendXmlData() {
-    // sprintf(buffer, "<V0>%d.%d</V0>\n", (int)(voltsA0),
-    // abs((int)(voltsA0 * 10) - ((int)(voltsA0)*10)));
-    // strcat(XML, buffer);
+// void sendMainPage() { server.send(200, "text/html", htmlWebPage); }
+// void sendXmlData() {
+//     // sprintf(buffer, "<V0>%d.%d</V0>\n", (int)(voltsA0),
+//     // abs((int)(voltsA0 * 10) - ((int)(voltsA0)*10)));
+//     // strcat(XML, buffer);
 
-    strcpy(XML, "<?xml version = '1.0'?>\n<Data>\n");
+//     strcpy(XML, "<?xml version = '1.0'?>\n<Data>\n");
 
-    if (isShowingStream) {
-        strcat(XML, "<StreamState>1</StreamState>\n");
-    } else {
-        strcat(XML, "<StreamState>0</StreamState>\n");
-    }
+//     if (isShowingStream) {
+//         strcat(XML, "<StreamState>1</StreamState>\n");
+//     } else {
+//         strcat(XML, "<StreamState>0</StreamState>\n");
+//     }
 
-    if (isListening) {
-        strcat(XML, "<ListeningState>1</ListeningState>\n");
-    } else {
-        strcat(XML, "<ListeningState>0</ListeningState>\n");
-    }
+//     if (isListening) {
+//         strcat(XML, "<ListeningState>1</ListeningState>\n");
+//     } else {
+//         strcat(XML, "<ListeningState>0</ListeningState>\n");
+//     }
 
-    if (isTalking) {
-        strcat(XML, "<TalkingState>1</TalkingState>\n");
-    } else {
-        strcat(XML, "<TalkingState>0</TalkingState>\n");
-    }
+//     if (isTalking) {
+//         strcat(XML, "<TalkingState>1</TalkingState>\n");
+//     } else {
+//         strcat(XML, "<TalkingState>0</TalkingState>\n");
+//     }
 
-    strcat(XML, "</Data>\n");
+//     strcat(XML, "</Data>\n");
 
-    server.send(200, "text/xml", XML);
-}
+//     server.send(200, "text/xml", XML);
+// }
 
-void toggleViewStream() {
-    isShowingStream = !isShowingStream;
-    isListening = false;
-    isTalking = false;
+// void toggleViewStream() {
+//     isShowingStream = !isShowingStream;
+//     isListening = false;
+//     isTalking = false;
 
-    server.send(200, "text/plain", "");
-}
-void toggleListening() {
-    isListening = !isListening;
-    isShowingStream = false;
-    isTalking = false;
+//     server.send(200, "text/plain", "");
+// }
+// void toggleListening() {
+//     isListening = !isListening;
+//     isShowingStream = false;
+//     isTalking = false;
 
-    server.send(200, "text/plain", "");
-}
-void toggleTalking() {
-    isTalking = !isTalking;
-    isShowingStream = false;
-    isListening = false;
+//     server.send(200, "text/plain", "");
+// }
+// void toggleTalking() {
+//     isTalking = !isTalking;
+//     isShowingStream = false;
+//     isListening = false;
 
-    server.send(200, "text/plain", "");
-}
-void sliderIntensityUpdate() {
-    String intensityString = server.arg("intensity");
-    ledIntensity = intensityString.toInt();
+//     server.send(200, "text/plain", "");
+// }
+// void sliderIntensityUpdate() {
+//     String intensityString = server.arg("intensity");
+//     ledIntensity = intensityString.toInt();
 
-    server.send(200, "text/plain", "");
-}
+//     server.send(200, "text/plain", "");
+// }
--- Programs/Tests/Fetch Stream/main.py
+++ Programs/Tests/Fetch Stream/main.py
@@ -1,6 +1,6 @@
 import cv2
 
-url = "http://192.168.244.202"
+url = "http://192.168.1.103"
 stream = url + ":81/stream"
 cap = cv2.VideoCapture(stream)
 # cap.set(3, 640)  # set width
@@ -10,11 +10,12 @@ while True:
     _, frame = cap.read()
     cv2.imshow("Stream", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord("s"):
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord("s"):
         cv2.imwrite("uxga__img.jpg", frame)
         print("Save")
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    if key & 0xFF == ord("q"):
         break
 
 cap.release()
