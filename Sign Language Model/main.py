d2737f62c58378bbddd1dee3b4f4acf5bbd40109
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-04-30 16:32:58 +0100

    Test the model with 200 epochs

--- Sign Language Model/main.py
+++ Sign Language Model/main.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
+import numpy as np
 import cvzone
 import os
 import cv2
 import math
 
-weight_path = os.path.join("Weights", "best.pt")
+weight_path = os.path.join("Weights", "best__200epochs.pt")
 model = YOLO(weight_path)
 
 classNames = ["thumsup", "thumsdown", "thankyou", "livelong"]
@@ -12,16 +13,21 @@ text_background = (198, 63, 88)
 corner_color = (53, 53, 249)
 text_color = (239, 239, 239)
 border_color = (61, 147, 8)
+text_message = ""
+list_message = []
 
-stream_url = "http://192.168.169.196:81/stream"
+stream_url = 0  # "http://192.168.169.196:81/stream"
 cap = cv2.VideoCapture(stream_url)
 
 frame_counter = 0
 while True:
     _, frame = cap.read()
+    frame = cv2.resize(frame, (640, 640))
+    frame = cv2.flip(frame, 1)
+    frame_copy = np.copy(frame)
 
-    if frame_counter % 30 == 0:
-        results = model(frame, stream=True)
+    if frame_counter % 10 == 0:
+        results = model(frame_copy, stream=True, max_det=1)
 
         for r in results:
             boxes = r.boxes
@@ -30,20 +36,26 @@ while True:
                 class_id = int(box.cls[0])
                 object_class = classNames[class_id].capitalize()
 
+                if object_class not in list_message:
+                    list_message.append(object_class)
+
+                text_message = " ".join(list_message)
+
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, w, h = int(x1), int(y1), int(x2 - x1), int(y2 - y1)
-                cvzone.cornerRect(
-                    frame,
-                    [x1, y1, w, h],
-                    t=3,
-                    rt=2,
-                    colorR=border_color,
-                    colorC=corner_color,
-                )
+                # cvzone.cornerRect(
+                #     frame_copy,
+                #     [x1, y1, w, h],
+                #     t=3,
+                #     rt=2,
+                #     colorR=border_color,
+                #     colorC=corner_color,
+                # )
                 cvzone.putTextRect(
-                    frame,
-                    f"{object_class}",
-                    pos=(max(8, x1 + 6), max(23, y1 - 12)),
+                    frame_copy,
+                    f"{text_message}",
+                    # pos=(max(8, x1 + 6), max(23, y1 - 12)),
+                    pos=(30, 30),
                     scale=1,
                     thickness=1,
                     colorT=text_color,
@@ -54,6 +66,8 @@ while True:
                     colorB=text_color,
                 )
 
+                cv2.imshow("Processed Frame", frame_copy)
+
     cv2.imshow("Frame", frame)
 
     frame_counter += 1
@@ -61,7 +75,11 @@ while True:
         frame_counter = 0
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord("c"):
+        list_message = []
+        text_message = ""
+    if key & 0xFF == ord("q"):
         break
 
 cap.release()
