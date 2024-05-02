e1a5eb572bbf767defc48f00321c03646690e5fe
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-05-02 14:09:11 +0100

    Resolve the problem for detections in specific frames

--- Sign Language Model/main.py
+++ Sign Language Model/main.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
-import numpy as np
 import cvzone
 import os
 import cv2
@@ -7,6 +6,7 @@ import math
 
 weight_path = os.path.join("Weights", "best__200epochs.pt")
 model = YOLO(weight_path)
+last_boxes = []
 
 classNames = ["thumsup", "thumsdown", "thankyou", "livelong"]
 text_background = (198, 63, 88)
@@ -22,51 +22,41 @@ cap = cv2.VideoCapture(stream_url)
 frame_counter = 0
 while True:
     _, frame = cap.read()
-    frame = cv2.resize(frame, (640, 640))
-    frame = cv2.flip(frame, 1)
-    frame_copy = np.copy(frame)
 
     if frame_counter % 10 == 0:
-        results = model(frame_copy, stream=True, max_det=1)
-
+        results = model(frame, stream=True, max_det=1)
         for r in results:
-            boxes = r.boxes
-            for box in boxes:
-                conf = math.floor((box.conf[0] * 100))
+            for box in r.boxes:
                 class_id = int(box.cls[0])
-                object_class = classNames[class_id].capitalize()
+                conf = math.floor(box.conf[0] * 100)
+                last_boxes = [
+                    (
+                        box,
+                        conf,
+                        class_id,
+                    )
+                ]
 
-                if object_class not in list_message:
-                    list_message.append(object_class)
+                if classNames[class_id] not in list_message:
+                    list_message.append(classNames[class_id])
 
-                text_message = " ".join(list_message)
-
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, w, h = int(x1), int(y1), int(x2 - x1), int(y2 - y1)
-                # cvzone.cornerRect(
-                #     frame_copy,
-                #     [x1, y1, w, h],
-                #     t=3,
-                #     rt=2,
-                #     colorR=border_color,
-                #     colorC=corner_color,
-                # )
-                cvzone.putTextRect(
-                    frame_copy,
-                    f"{text_message}",
-                    # pos=(max(8, x1 + 6), max(23, y1 - 12)),
-                    pos=(30, 30),
-                    scale=1,
-                    thickness=1,
-                    colorT=text_color,
-                    colorR=text_background,
-                    font=cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                    offset=8,
-                    border=1,
-                    colorB=text_color,
-                )
-
-                cv2.imshow("Processed Frame", frame_copy)
+    for box, conf, class_id in last_boxes:
+        if len(list_message) > 0:
+            text_message = " ".join(list_message)
+            cvzone.putTextRect(
+                frame,
+                f"{text_message}",
+                # pos=(max(8, x1 + 6), max(23, y1 - 12)),
+                pos=(30, 30),
+                scale=1,
+                thickness=1,
+                colorT=text_color,
+                colorR=text_background,
+                font=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                offset=8,
+                border=1,
+                colorB=text_color,
+            )
 
     cv2.imshow("Frame", frame)
 
